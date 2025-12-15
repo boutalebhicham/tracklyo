0d77adf1ec71c4338d46e642c1614e273af3f043
Hicham Boutaleb <boutalebhicham.mail@gmail.com>
2025-12-15 17:43:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8232031369-54743",
+  "appId": "1:306226706582:web:10b8a5308a1ee5d8e0aab7",
+  "apiKey": "AIzaSyArhE7KQTQqDxO8fS9FYY65Jis-ObZ6qK4",
+  "authDomain": "studio-8232031369-54743.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "306226706582"
+};
